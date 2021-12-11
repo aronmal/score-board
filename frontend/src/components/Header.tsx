29a621ffeed14e0ed6625f1578f669d114484605
aronmal <aron@mal-noh.de>
2021-12-11 19:36:09 +0100

    added RTC (real-time clock)

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,16 +1,26 @@
+import { useState, useEffect } from "react";
 import Nav from './Nav';
+import clock from "./Clock";
 
 function Header() {
-    return (
-      <header className="Nav-header">
-        <p id='top-banner'>This webpage is under construction and not finished yet!</p>
-        <div id='top-header'>
-          <p id='logo'>Score:Board</p>
-          <Nav />
-          <p id='time'>19:52</p>
-        </div>
-      </header>
-    );
-  }
+  const [time, setTime] = useState('00:00');
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(() => clock());
+    }, 1000);
+  });
+
+  return (
+    <header className="Nav-header">
+      <p id='top-banner'>This webpage is under construction and is not finished yet!</p>
+      <div id='top-header'>
+        <p id='logo'>Score:Board</p>
+        <Nav />
+        <p id='time'> {time} </p>
+      </div>
+    </header>
+  );
+}
 
 export default Header;
--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,15 +1,15 @@
 import {Link} from 'react-router-dom'
 
 function Nav() {
-    return (
-      <nav>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/test' className='active'>Test</Link></li>
-          <li><Link to='/login'>Login</Link></li>
-        </ul>
-      </nav>
-    );
-  }
+  return (
+    <nav>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/test' className='active'>Test</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+      </ul>
+    </nav>
+  );
+}
 
 export default Nav;
