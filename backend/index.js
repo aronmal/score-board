199a0fd20e4b4c1494cc969a9f6909ef801aa93b
aronmal <aron@mal-noh.de>
2021-12-12 18:50:04 +0100

    Added functional backend with frontend config test

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,63 @@
-console.log("hello")
-console.log("what the heck")
+const cors = require('cors');
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+var configFileData
+var newConfigFileData
+
+// Start listening on port 5000
+app.listen(5000, () => console.log('Server running on: http://localhost:5000'))
+app.use(express.static('public'))
+app.use(express.json())
+app.use(cors());
+
+app.get('/api/get', (req,res) => {
+    readConfig()
+    res.send(JSON.parse(configFileData))
+    console.log('[GET] Request served')
+})
+app.post('/api/post', (req,res) => {
+    readConfig()
+    console.log('[POST] Recieving request:')
+    newConfigFileData = req.body
+    console.log(req.body)
+    console.log('[POST] Request received.')
+    saveConfig()
+    res.json({
+        status: 'success',
+        message: 'received'
+    })
+})
+
+
+
+
+async function readConfig() {
+    try {
+        configFileData = fs.readFileSync('config.json', 'utf8')
+      } catch (err) {
+        console.error(err)
+      }
+}
+
+// if new configuration, write to file
+function saveConfig() {
+    if ((configFileData !== newConfigFileData) && (newConfigFileData !== undefined))  {
+        try {
+            console.log('[Info] Saving new config ...')
+            fs.writeFileSync('config.json', JSON.stringify(newConfigFileData))
+            //file written successfully
+            console.log('[Info] Data written, config saved!')
+        } catch (err) {
+            //error message
+            console.error(err)
+        }
+    } else if (configFileData !== newConfigFileData) {
+        console.log('No writing action, new config and config file are the same.')
+    } else if (newConfigFileData == undefined) {
+        console.log('No writing action, new config is undefined')
+    } else {
+        console.log('New config not saved, an unknown error accured!')
+    }
+}
--- frontend/src/components/Test.tsx
+++ frontend/src/components/Test.tsx
@@ -1,11 +1,72 @@
-function Test() {
-  return (
+import { useEffect, useState } from "react";
 
-    <div className="Test">
-      <p>
-        This is a Test!!!
-      </p>
-    </div>
+function Test() {
+  
+  const [testconfig, setTestconfig] = useState('<p>Loading ...</p>');
+
+  useEffect(() => {
+
+    var fetchPassed:Boolean = false;
+
+    const getOptions = {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json'
+      }
+    };
+
+    const loadTest= async () => {
+      const response = await fetch('http://localhost:5000/api/get', getOptions)
+      .then(res => {
+        fetchPassed = true
+        console.log('[INFO] Received configData');
+        return res.json()
+      })
+      .catch(error => {
+          console.log(error)
+          console.log('[WARN] No Connection to Backend/API')
+          console.log('[ERROR] Fehler bei abrufen der Konfiguration!')
+      });
+      return response;
+    };
+
+    loadTest().then(result => {
+      setTestconfig(() => {
+        if (fetchPassed) {
+          var jsonVar = result,
+          jsonStr = JSON.stringify(jsonVar),
+          regeStr = '',
+          f = {
+                  brace: 0
+              }; // for tracking matches, in particular the curly braces
+          
+          regeStr = jsonStr.replace(/({|}[,]*|[^{}:]+:[^{}:,]*[,{]*)/g, function (m, p1) {
+              var rtnFn = function() {
+                      return '<div style="text-indent: ' + (f['brace'] * 20) + 'px;">' + p1 + '</div>';
+                  },
+                  rtnStr:any = 0;
+              if (p1.lastIndexOf('{') === (p1.length - 1)) {
+                  rtnStr = rtnFn();
+                  f['brace'] += 1;
+              } else if (p1.indexOf('}') === 0) {
+                  f['brace'] -= 1;
+                  rtnStr = rtnFn();
+              } else {
+                  rtnStr = rtnFn();
+              }
+              return rtnStr;
+          });
+          return regeStr
+        }
+        return '<p>An error occurred while fetching the config data!</p>'
+      });
+      console.log(`[INFO] Config pasted!`);
+    });
+
+  }, []);
+
+  return (
+    <div dangerouslySetInnerHTML={{__html: testconfig}} />
   );
 }
 
