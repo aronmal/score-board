6968f96d89b7d17642088db27f02a41e164c900d
aronmal <aron@mal-noh.de>
2022-01-10 12:04:20 +0100

    Logout quick fix

--- frontend/src/components/Logout.tsx
+++ frontend/src/components/Logout.tsx
@@ -9,34 +9,35 @@ function Logout() {
 
     useEffect(() => {
         logout()
-    }, [])
 
-    async function logout() {
-        if (!isLoggedIn)
-            setElem(<Navigate to='/' />);
-        const res: void | Response = await fetch('/api/logout', {
-            method: 'DELETE',
-            headers: { 'content-type': 'application/json' },
-        }).catch((err: Error) => {
-            console.log(err)
-            setElem(<p style={{color: 'red'}}>{ err.toString() }</p>)
-            setTimeout(() => {
-                setElem(<></>)
-            }, 5000)
-        })
-        if (!res)
-            return;
-        if (res.status === 200) {
-            setIsLoggedIn(false)
-            setElem(<Navigate to='/' />)
-        } else {
-            console.log(res)
-            setElem(<p style={{color: 'red'}}>{ 'Error ' + res.status + ' ' + res.statusText }</p>)
-            setTimeout(() => {
-                setElem(<></>)
-            }, 5000)
+        async function logout() {
+            if (!isLoggedIn)
+                setElem(<Navigate to='/' />);
+            const res: void | Response = await fetch('/api/logout', {
+                method: 'DELETE',
+                headers: { 'content-type': 'application/json' },
+            }).catch((err: Error) => {
+                console.log(err)
+                setElem(<p style={{color: 'red'}}>{ err.toString() }</p>)
+                setTimeout(() => {
+                    setElem(<></>)
+                }, 5000)
+            })
+            if (!res)
+                return;
+            if (res.status === 200) {
+                setIsLoggedIn(false)
+                setElem(<Navigate to='/' />)
+            } else {
+                console.log(res)
+                setElem(<p style={{color: 'red'}}>{ 'Error ' + res.status + ' ' + res.statusText }</p>)
+                setTimeout(() => {
+                    setElem(<></>)
+                }, 5000)
+            }
         }
-    }
+    }, [isLoggedIn, setIsLoggedIn])
+
         
     return elem
 }
