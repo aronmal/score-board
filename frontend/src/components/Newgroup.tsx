4e234054b6079e58f37cf4ad05ea0ebedbed2fc3
aronmal <aron@mal-noh.de>
2022-01-17 19:10:53 +0100

    Little bit improvement

--- frontend/src/components/Newgroup.tsx
+++ frontend/src/components/Newgroup.tsx
@@ -127,6 +127,7 @@ function New() {
   const checkForTeamnameDuplicates = (teamname: string) => (teams.filter(e => e.name === teamname).length >= 2)
   const checkForPlayernameDuplicates = (playerUuid: string) => (players[players.findIndex(e => e.uuid === playerUuid)].name !== '') ? (players.filter(e => e.name === players[players.findIndex(e => e.uuid === playerUuid)].name).length >= 2) : false
   const playernameOfUuid = (playerUuid: string) => players[players.findIndex(e => e.uuid === playerUuid)].name
+  const isLastPlayerInTeam = (team: teamType, playerUuid: string) => (team.players[team.players.length - 1] === playerUuid)
 
   const addPlayerToTeam = (teamUuid: string) => {
     const playerUuid = uuidv4()
@@ -136,6 +137,8 @@ function New() {
   };
 
   const changePlayername = (e: React.ChangeEvent<HTMLInputElement>, playerUuid: string) => {
+    if (e.target.value.length !<= 20)
+      return;
     const match = players.findIndex((e) => e.uuid === playerUuid)
     setPlayers((prev) => Object.assign([...prev], { [match]: {...prev[match], name: validate(e.target.value)}}))
   };
@@ -168,6 +171,8 @@ function New() {
   };
 
   const changeTeamname = (e: React.ChangeEvent<HTMLInputElement>, teamUuid: string) => {
+    if (e.target.value.length !<= 20 || (e.target.value === ''))
+      return;
     const match = teams.findIndex((e) => e.uuid === teamUuid)
     setTeams((prev) => Object.assign([...prev], { [match]: {...prev[match], name: validate(e.target.value)}}))
   };
@@ -177,7 +182,7 @@ function New() {
     setTeams((prev) => [...prev.filter((_, i) => i !== match)])
   };
 
-  if (!isLoggedIn) return <Navigate to='/' />;
+  // if (!isLoggedIn) return <Navigate to='/' />;
 
   return (
     <div className='flex-col step-form' style={{'--playername-columns': playernameColumns} as CSSProperties}>
@@ -314,10 +319,7 @@ function New() {
                   type="text"
                   value={playernameOfUuid(playerUuid)}
                   placeholder='Spielername'
-                  onChange={e => {
-                    if (e.target.value.length <= 20)
-                      changePlayername(e, playerUuid);
-                  }}
+                  onChange={e => changePlayername(e, playerUuid)}
                   onKeyDown={e => {
                     if (e.code === 'Delete')
                       removePlayerFromTeam(teamUuid, playerUuid);
@@ -352,26 +354,21 @@ function New() {
         </div>
         <div className='team-content'>
           <div className="flex-col team-list">
-            {teams.map((team) => {
-              const { uuid: teamUuid, name: teamname, players: teamPlayers } = team;
-              return (
-              <div key={ teamUuid } className='flex-col team-div team-div-real'>
+            {teams.map((team) => (
+              <div key={ team.uuid } className='flex-col team-div team-div-real'>
                 <div className='flex-row'>
                   <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24"><path d="M17.997 18h-11.995l-.002-.623c0-1.259.1-1.986 1.588-2.33 1.684-.389 3.344-.736 2.545-2.209-2.366-4.363-.674-6.838 1.866-6.838 2.491 0 4.226 2.383 1.866 6.839-.775 1.464.826 1.812 2.545 2.209 1.49.344 1.589 1.072 1.589 2.333l-.002.619zm4.811-2.214c-1.29-.298-2.49-.559-1.909-1.657 1.769-3.342.469-5.129-1.4-5.129-1.265 0-2.248.817-2.248 2.324 0 3.903 2.268 1.77 2.246 6.676h4.501l.002-.463c0-.946-.074-1.493-1.192-1.751zm-22.806 2.214h4.501c-.021-4.906 2.246-2.772 2.246-6.676 0-1.507-.983-2.324-2.248-2.324-1.869 0-3.169 1.787-1.399 5.129.581 1.099-.619 1.359-1.909 1.657-1.119.258-1.193.805-1.193 1.751l.002.463z"/></svg>
                   <input
                     className='teamname-input input-box'
-                    style={checkForTeamnameDuplicates(teamname) ? {color: 'red'} : {}}
+                    style={checkForTeamnameDuplicates(team.name) ? {color: 'red'} : {}}
                     type='text'
-                    value={teamname}
+                    value={team.name}
                     placeholder='Teamname'
-                    onChange={e => {
-                      if (e.target.value.length <= 20 && (e.target.value !== ''))
-                        changeTeamname(e, teamUuid);
-                    }}
+                    onChange={e => changeTeamname(e, team.uuid)}
                   />
                   <button
                     className='team-minus-button input-box'
-                    onClick={() => removeTeam(teamUuid)}
+                    onClick={() => removeTeam(team.uuid)}
                   ><span>-</span></button>
                 </div>
                 <div className="grid-team-playernames">
@@ -385,32 +382,33 @@ function New() {
                         list="namelist"
                         placeholder='Spielername'
                         onChange={e => {
-                          if ((team.players[teamPlayers.length - 1] === playerUuid) && !playernameOfUuid(playerUuid) && e.target.value)
-                            addPlayerToTeam(teamUuid);
+                          if ((isLastPlayerInTeam(team, playerUuid)) && !playernameOfUuid(playerUuid) && e.target.value)
+                            addPlayerToTeam(team.uuid);
                           if (!!players.find(v => v.uuid === e.target.value))
-                            replacePlayerWithUuidTo(e, teamUuid, playerUuid);
-                          if (e.target.value.length <= 20)
-                            changePlayername(e, playerUuid);
+                            replacePlayerWithUuidTo(e, team.uuid, playerUuid);
+                          changePlayername(e, playerUuid);
                         }}
                         onKeyDown={e => {
-                          if (e.code === 'Delete')
-                            removePlayerFromTeam(teamUuid, playerUuid);
+                          if ((!isLastPlayerInTeam(team, playerUuid)) && (e.code === 'Delete'))
+                            removePlayerFromTeam(team.uuid, playerUuid);
                         }}
                         onBlur={() => {
-                          if ((team.players[teamPlayers.length - 1] !== playerUuid) && !playernameOfUuid(playerUuid))
-                            removePlayerFromTeam(teamUuid, playerUuid);
+                          if ((!isLastPlayerInTeam(team, playerUuid)) && !playernameOfUuid(playerUuid))
+                            removePlayerFromTeam(team.uuid, playerUuid);
                         }}
                       />
-                      <button
-                        tabIndex={-1}
-                        className='team-player-minus-button'
-                        onClick={() => removePlayerFromTeam(teamUuid, playerUuid)}
-                      ><span>{ '\u2A2F' }</span></button>
+                      {(!isLastPlayerInTeam(team, playerUuid)) ?
+                        <button
+                          tabIndex={-1}
+                          className='team-player-minus-button'
+                          onClick={() => removePlayerFromTeam(team.uuid, playerUuid)}
+                        ><span>{ '\u2A2F' }</span></button>
+                      : <></>}
                     </div>
                   ))}
                 </div>
               </div>
-            )})}
+            ))}
             <datalist id="namelist">
               {[...players.filter((v) => !v.team)].map(({ uuid, name }) =>
                 <option key={ uuid } value={ uuid }>{ name }</option>
