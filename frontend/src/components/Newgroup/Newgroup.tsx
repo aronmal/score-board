284be2bb24fb7a5628fcab8ad313da575930603d
aronmal <aron@mal-noh.de>
2022-01-25 17:28:39 +0100

    Production quick fix

--- frontend/src/components/Newgroup/Newgroup.tsx
+++ frontend/src/components/Newgroup/Newgroup.tsx
@@ -96,7 +96,7 @@ function New() {
     }
   }
 
-  // if (!isLoggedIn) return <Navigate to='/' />;
+  if (!isLoggedIn) return <Navigate to='/' />;
 
   return (
     <div className='flex-col step-form' style={{'--playername-columns': playernameColumns} as CSSProperties}>
