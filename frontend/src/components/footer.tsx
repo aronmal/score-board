5b2120070286b37d01bfe1ec4f1c327f3b5bd0cf
aronmal <aron@mal-noh.de>
2022-01-29 20:13:39 +0100

    Updated copyright year

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer>
             <a href="https://mal-noh.de/impressum" className="link" target="_blank" rel="noreferrer">Impressum</a>
-            <p>© 2021 - <span id="secret">Aron Malcher</span></p>
+            <p>© 2022 - <span id="secret">Aron Malcher</span></p>
         </footer>
     );
 }
